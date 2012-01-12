77076d5dca71ed31eeefcb0dd8c19c4e811807b8
Pierre Ossman <ossman@cendio.se>
2012-01-12 15:00:37 +0100

    Add another test case with mixed spaces and tabs.

---
+++ tests/mixed.c
@@ -0,0 +1,11 @@
+/* This file uses a mix of tabs and spaces for indentation */
+
+int main(int argc, char **argv)
+{
+	printf("Hello world!\n");
+	if (argc >= 2) {
+                printf("Your first argument is %s\n",
+                       argv[0]);
+                printf("\n");
+	}
+}
